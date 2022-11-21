e0698c95a77d9b441a6df96faee1011541d9b179
Muhammad Hassan <hassaniq@live.com>
2022-11-21 16:32:25 -0500

    Integrated Axios and fetched data from BE

--- src/pages/search/searchflight.tsx
+++ src/pages/search/searchflight.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Searchflight() 
-{
+type XResponse = {
+  Message: string;
+};
+axios.defaults.baseURL = "http://localhost:3500";
+function Searchflight(): JSX.Element {
+  const [x, setX] = useState<XResponse>({ Message: "Hello" });
+  useEffect(() => {
+    document.title = "National Quick Travel";
+    axios.get("/").then((res) => {
+      setX(res.data);
+      console.log("Hi");
+    });
+  }, [0]);
   return (
-    <div>Search Flight</div>
-  )
+    <>
+      <div>Search Flight</div>
+      <div>{x.Message}</div>
+    </>
+  );
 }
 
 export default Searchflight;
